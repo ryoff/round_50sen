caef943142ad74a2c46f3920aa36cce5f76eca7f
ryoff <ryoffes@gmail.com>
2015-02-10 13:35:03 +0900

    BigDecimalにて引数が存在した場合の対応を追加

--- lib/round_50sen/core_ext/bigdecimal.rb
+++ lib/round_50sen/core_ext/bigdecimal.rb
@@ -1,8 +1,11 @@
 require 'bigdecimal'
 class BigDecimal
   def round_50sen(n = 0)
-    return (self - BigDecimal('0.5')).ceil(n) if self >= 0
+    digits = n >= 0 ? '0.1' : '10'
+    adjuster = BigDecimal('0.5') * BigDecimal(digits) ** n.abs
 
-    (self + BigDecimal('0.5')).floor(n)
+    return (self - adjuster).ceil(n) if self >= 0
+
+    (self + adjuster).floor(n)
   end
 end
--- spec/round_50sen_spec.rb
+++ spec/round_50sen_spec.rb
@@ -47,31 +47,110 @@ describe "#round_50sen " do
   end
 
   describe BigDecimal do
-    where(:big_decimal, :rounded) do
-      [
-        [BigDecimal('0.50'), 0],
-        [BigDecimal('0.51'), 1],
-        [BigDecimal('1.49'), 1],
-        [BigDecimal('1.50'), 1],
-        [BigDecimal('1.51'), 2],
-        [BigDecimal('1.50000000'), 1],
-        [BigDecimal('1.50000001'), 2],
-        [BigDecimal('1.50000000000000000000000000000000000000'), 1],
-        [BigDecimal('1.50000000000000000000000000000000000001'), 2],
-        [BigDecimal('-0.50'), -0],
-        [BigDecimal('-0.51'), -1],
-        [BigDecimal('-1.49'), -1],
-        [BigDecimal('-1.50'), -1],
-        [BigDecimal('-1.51'), -2],
-        [BigDecimal('-1.5000000000000000000000000000000000000'), -1],
-        [BigDecimal('-1.5000000000000000000000000000000000001'), -2],
-      ]
+    context 'without args' do
+      where(:big_decimal, :rounded) do
+        [
+          [BigDecimal('0.50'), 0],
+          [BigDecimal('0.51'), 1],
+          [BigDecimal('1.49'), 1],
+          [BigDecimal('1.50'), 1],
+          [BigDecimal('1.51'), 2],
+          [BigDecimal('1.50000000'), 1],
+          [BigDecimal('1.50000001'), 2],
+          [BigDecimal('1.50000000000000000000000000000000000000'), 1],
+          [BigDecimal('1.50000000000000000000000000000000000001'), 2],
+          [BigDecimal('-0.50'), -0],
+          [BigDecimal('-0.51'), -1],
+          [BigDecimal('-1.49'), -1],
+          [BigDecimal('-1.50'), -1],
+          [BigDecimal('-1.51'), -2],
+          [BigDecimal('-1.5000000000000000000000000000000000000'), -1],
+          [BigDecimal('-1.5000000000000000000000000000000000001'), -2],
+        ]
+      end
+
+      with_them do
+        subject { big_decimal.round_50sen }
+
+        it { is_expected.to eq rounded }
+      end
     end
 
-    with_them do
-      subject { big_decimal.round_50sen }
+    context 'with args is 4' do
+      where(:big_decimal, :rounded) do
+        [
+          [BigDecimal('0.50'), 0.5],
+          [BigDecimal('0.51'), 0.51],
+          [BigDecimal('1.49'), 1.49],
+          [BigDecimal('1.50'), 1.5],
+          [BigDecimal('1.51'), 1.51],
+          [BigDecimal('1.50000000'), 1.5],
+          [BigDecimal('1.50000001'), 1.5],
+          [BigDecimal('1.55555000'), 1.5555],
+          [BigDecimal('1.55555001'), 1.5556],
+          [BigDecimal('1.55555000000000000000000000000000000000'), 1.5555],
+          [BigDecimal('1.55555000000000000000000000000000000001'), 1.5556],
+          [BigDecimal('-0.50'), -0.5],
+          [BigDecimal('-0.51'), -0.51],
+          [BigDecimal('-1.49'), -1.49],
+          [BigDecimal('-1.50'), -1.50],
+          [BigDecimal('-1.51'), -1.51],
+          [BigDecimal('-1.55555000'), -1.5555],
+          [BigDecimal('-1.55555001'), -1.5556],
+          [BigDecimal('-1.55555000000000000000000000000000000000'), -1.5555],
+          [BigDecimal('-1.55555000000000000000000000000000000001'), -1.5556],
+        ]
+      end
 
-      it { is_expected.to eq rounded }
+      with_them do
+        subject { big_decimal.round_50sen(4) }
+
+        it { is_expected.to eq rounded }
+      end
+    end
+
+    context 'with args is minus 1' do
+      where(:big_decimal, :rounded) do
+        [
+          [BigDecimal('0.50'), 0],
+          [BigDecimal('15.0'), 10],
+          [BigDecimal('15.1'), 20],
+          [BigDecimal('15.000000000000000000000000000000000'), 10],
+          [BigDecimal('15.000000000000000000000000000000001'), 20],
+          [BigDecimal('-15.0'), -10],
+          [BigDecimal('-15.1'), -20],
+          [BigDecimal('-15.000000000000000000000000000000000'), -10],
+          [BigDecimal('-15.000000000000000000000000000000001'), -20],
+        ]
+      end
+
+      with_them do
+        subject { big_decimal.round_50sen(-1) }
+
+        it { is_expected.to eq rounded }
+      end
+    end
+
+    context 'with args is minus 4' do
+      where(:big_decimal, :rounded) do
+        [
+          [BigDecimal('0.50'), 0],
+          [BigDecimal('15000.0'), 10000],
+          [BigDecimal('15000.1'), 20000],
+          [BigDecimal('15000.000000000000000000000000000000000'), 10000],
+          [BigDecimal('15000.000000000000000000000000000000001'), 20000],
+          [BigDecimal('-15000.0'), -10000],
+          [BigDecimal('-15000.1'), -20000],
+          [BigDecimal('-15000.000000000000000000000000000000000'), -10000],
+          [BigDecimal('-15000.000000000000000000000000000000001'), -20000],
+        ]
+      end
+
+      with_them do
+        subject { big_decimal.round_50sen(-4) }
+
+        it { is_expected.to eq rounded }
+      end
     end
   end
 end
